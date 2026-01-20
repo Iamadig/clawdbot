e8b086667d4482d95e03eab6db8807b042e694be
AG <adig7675@gmail.com>
2026-01-20 15:23:31 -0800

    chore: hard reset vps repo before pull

--- deploy_vps.sh
+++ deploy_vps.sh
@@ -8,7 +8,7 @@ VPS_HOST="${CLAWDBOT_VPS_HOST:-65.108.48.234}"
 echo ">>> Deploying Remote Node Configuration to $VPS_HOST..."
 
 echo ">>> Updating repo on VPS..."
-ssh -i ~/.ssh/hetzner_worker "$VPS_USER@$VPS_HOST" "cd /root/clawdbot && if [ -d .git ]; then git fetch origin && git checkout custom/prod && git pull --rebase origin main; else echo 'No git repo found at /root/clawdbot'; fi"
+ssh -i ~/.ssh/hetzner_worker "$VPS_USER@$VPS_HOST" "cd /root/clawdbot && if [ -d .git ]; then git reset --hard && git fetch origin && git checkout custom/prod && git pull --rebase origin main; else echo 'No git repo found at /root/clawdbot'; fi"
 
 echo ">>> Uploading configuration files..."
 scp -i ~/.ssh/hetzner_worker -r Dockerfile docker-compose.yml *_config.json vps_setup_script.sh vps_skills "$VPS_USER@$VPS_HOST:/root/clawdbot/"
