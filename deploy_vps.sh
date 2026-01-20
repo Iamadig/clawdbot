afaaff8b6a3dc022cc258fd3724f06ad195221e4
AG <adig7675@gmail.com>
2026-01-20 15:19:51 -0800

    chore: sync vps repo before upload

--- deploy_vps.sh
+++ deploy_vps.sh
@@ -7,11 +7,11 @@ VPS_HOST="${CLAWDBOT_VPS_HOST:-65.108.48.234}"
 
 echo ">>> Deploying Remote Node Configuration to $VPS_HOST..."
 
-echo ">>> Uploading configuration files..."
-scp -i ~/.ssh/hetzner_worker -r Dockerfile docker-compose.yml *_config.json vps_setup_script.sh vps_skills "$VPS_USER@$VPS_HOST:/root/clawdbot/"
-
 echo ">>> Updating repo on VPS..."
 ssh -i ~/.ssh/hetzner_worker "$VPS_USER@$VPS_HOST" "cd /root/clawdbot && if [ -d .git ]; then git fetch origin && git checkout custom/prod && git pull --rebase origin main; else echo 'No git repo found at /root/clawdbot'; fi"
+
+echo ">>> Uploading configuration files..."
+scp -i ~/.ssh/hetzner_worker -r Dockerfile docker-compose.yml *_config.json vps_setup_script.sh vps_skills "$VPS_USER@$VPS_HOST:/root/clawdbot/"
 
 echo ">>> Executing setup on VPS..."
 ssh -i ~/.ssh/hetzner_worker "$VPS_USER@$VPS_HOST" "chmod +x /root/clawdbot/vps_setup_script.sh && /root/clawdbot/vps_setup_script.sh"
